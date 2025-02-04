4bea4983e865d5d4316ef549e2713900df8ef48e
Anjan Mahapatra <anjanmahapatra10@gmail.com>
2025-02-05 00:06:26 +0530

    Roll_Cage_Simulation.py

---
+++ Roll_Cage_Simulation.py
@@ -0,0 +1,84 @@
+import numpy as np
+import meshio
+import matplotlib.pyplot as plt
+from scipy.spatial import KDTree
+from scipy.linalg import eigh
+
+class RollCageSimulation:
+    def __init__(self, mesh_file):
+        self.mesh = meshio.read(mesh_file)
+        self.nodes = np.array(self.mesh.points)
+        self.elements = np.array(self.mesh.cells_dict['tetra'])  # Assuming tetrahedral mesh
+        self.material_properties = {}
+        self.boundary_conditions = {}
+        self.loads = {}
+        self.stresses = np.zeros(len(self.elements))  # Placeholder for stress values
+
+    def set_material_properties(self, youngs_modulus, poisson_ratio, density):
+        self.material_properties = {
+            'E': youngs_modulus,
+            'nu': poisson_ratio,
+            'rho': density
+        }
+
+    def apply_boundary_conditions(self, fixed_nodes):
+        self.boundary_conditions['fixed'] = fixed_nodes
+
+    def apply_loads(self, force_nodes, force_values):
+        self.loads['nodes'] = force_nodes
+        self.loads['forces'] = force_values
+    
+    def stiffness_matrix(self):
+        num_nodes = len(self.nodes)
+        K = np.zeros((3*num_nodes, 3*num_nodes))
+        # Placeholder: Implement FEM assembly here
+        return K
+    
+    def mass_matrix(self):
+        num_nodes = len(self.nodes)
+        M = np.zeros((3*num_nodes, 3*num_nodes))
+        # Placeholder: Implement mass matrix assembly here
+        return M
+    
+    def modal_analysis(self, num_modes=6):
+        K = self.stiffness_matrix()
+        M = self.mass_matrix()
+        evals, evecs = eigh(K, M, subset_by_index=[0, num_modes-1])
+        return evals, evecs
+    
+    def factor_of_safety(self):
+        yield_strength = float(input("Enter Yield Strength of Material (Pa): "))
+        self.stresses = np.random.uniform(0.1 * yield_strength, 0.9 * yield_strength, len(self.elements))  # Placeholder stress values
+        fos = yield_strength / self.stresses
+        return fos
+    
+    def plot_factor_of_safety(self):
+        fos = self.factor_of_safety()
+        plt.figure()
+        plt.hist(fos, bins=20, edgecolor='black')
+        plt.xlabel('Factor of Safety')
+        plt.ylabel('Frequency')
+        plt.title('Factor of Safety Distribution')
+        plt.show()
+    
+    def visualize(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(self.nodes[:, 0], self.nodes[:, 1], self.nodes[:, 2], s=1)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
+
+if __name__ == "__main__":
+    youngs_modulus = float(input("Enter Young's Modulus (Pa): "))
+    poisson_ratio = float(input("Enter Poisson's Ratio: "))
+    density = float(input("Enter Density (kg/m^3): "))
+    
+    sim = RollCageSimulation('rollcage_mesh.vtk')
+    sim.set_material_properties(youngs_modulus, poisson_ratio, density)
+    sim.apply_boundary_conditions(fixed_nodes=[0, 1, 2])  # Example
+    sim.apply_loads(force_nodes=[10, 11], force_values=[[0, 0, -1000], [0, 0, -1000]])
+    eigenvalues, eigenvectors = sim.modal_analysis()
+    sim.visualize()
+    sim.plot_factor_of_safety()
